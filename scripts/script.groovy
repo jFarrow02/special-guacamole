666112b60224f0f70fb1f9725ff82e61632f250b
jfarrow02 <jack.dempsey.farrow@gmail.com>
2022-06-04 17:25:40 -0400

    corrects --password-stdin sytnax

--- scripts/script.groovy
+++ scripts/script.groovy
@@ -2,7 +2,7 @@ def buildImage() {
     echo 'building image...'
       withCredentials([usernamePassword(credentialsId: 'nexus-creds', usernameVariable: 'USER', passwordVariable: 'PWD')]){
         sh "docker build -t $IMAGE_NAME ."
-        sh "echo $PWD | docker login -u $USER --password stdin $NEXUS_IP:$NEXUS_PORT"
+        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS_IP:$NEXUS_PORT"
         sh "docker push $IMAGE_NAME"
     }
 }
